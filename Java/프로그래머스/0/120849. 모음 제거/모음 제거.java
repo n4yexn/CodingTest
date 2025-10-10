5dccfa049c4720eaf62a70ddb311dff8c437b3a2
n4yexn <n4yexn@gmail.com>
2025-10-11 02:39:26 +0900

    [level 0] Title: 모음 제거, Time: 1.68 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++) {
+            char str = my_string.charAt(i);
+            
+            if(str != 'a' && str != 'e'&& str != 'i'&& str != 'o'&& str != 'u') {
+                answer += String.valueOf(str);
+            }
+        }
+            
+        return answer;
+    }
+}
