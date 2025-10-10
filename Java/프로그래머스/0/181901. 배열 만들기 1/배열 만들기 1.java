2326e5ee2a8df66f5889ca3814c5afae13789332
n4yexn <n4yexn@gmail.com>
2025-10-11 00:10:11 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.08 ms, Memory: 87.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,14 +1,10 @@
 class Solution {
     public int[] solution(int n, int k) {
         int[] answer = new int[n/k];
-        int index = 0;
         
-        for(int i=1; i<n+1; i++) {
-           if(i%k == 0) {
-               answer[index] = i;
-               index++;
+        for(int i=1; i<n/k+1; i++) {
+           answer[i-1] = k*i;
            }
-        }
         
         return answer;
     }
