351f8c73f95d25090c33cd89c73ea901c3663bc3
n4yexn <n4yexn@gmail.com>
2026-01-30 20:19:31 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 2.09 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        
+        for(String i : arr) {
+            answer += i;
+        }
+       
+        return answer;
+    }
+}
