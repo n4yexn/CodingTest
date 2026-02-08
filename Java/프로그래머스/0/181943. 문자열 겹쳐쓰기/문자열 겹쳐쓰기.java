32c02b4fc23017e8c1803baadaf5e21701553e82
n4yexn <n4yexn@gmail.com>
2026-02-08 11:55:28 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 14.56 ms, Memory: 95.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ Java/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
         String a = my_string.substring(0, s);
-        String b = my_string.substring(s+overwrite_string.length(), my_string.length());
+        String b = my_string.substring(s+overwrite_string.length());
        
         answer = a + overwrite_string + b;
         return answer;
