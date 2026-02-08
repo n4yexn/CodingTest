d9d08c25d48ea8e15af9b8e81b9ea43af4cceb5f
n4yexn <n4yexn@gmail.com>
2026-02-09 01:39:33 +0900

    [level 0] Title: 문자열 섞기, Time: 14.55 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        
+        char a [] = str1.toCharArray();
+        char b [] = str2.toCharArray();
+        
+        for(int i=0; i<str1.length(); i++) {
+            answer += String.valueOf(str1.charAt(i)) + String.valueOf(str2.charAt(i)); 
+        }
+        
+        return answer;
+    }
+}
