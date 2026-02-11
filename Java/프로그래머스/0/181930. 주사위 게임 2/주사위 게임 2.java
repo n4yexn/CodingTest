4a900214242cfa9d384411b492c1d35921c4d5a1
n4yexn <n4yexn@gmail.com>
2026-02-12 01:01:18 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ Java/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
+        int answer = a+b+c;
         
-        if(a != b && b != c && c != a)
-            answer = a + b + c;
-        else if((a == b && b != c && c != a) || (a != b && b == c && c != a) || (a != b && b != c && c == a))
-            answer = (a + b + c) * (a*a + b*b + c*c);
-        else if(a == b && b == c && c == a)
-            answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c) ;
-        return answer;
+        if(a == b && b == c)
+            return answer * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c) ;
+        else if(a == b || b == c || c == a)
+            return answer * (a*a + b*b + c*c);
+        
+        else
+            return answer;
     }
 }
