1bf0bf6592c069fec6a267fce8193a2f06dc7793
n4yexn <n4yexn@gmail.com>
2026-02-14 02:33:42 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[arr.length];
+        
+        for(int i=0; i<queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            
+            int t = arr[a];
+            arr[a] = arr[b];
+            arr[b] = t;
+        }
+        return arr;
+    }
+}
