87ead59950ce7f1523bc30c67a108209c3223848
n4yexn <n4yexn@gmail.com>
2026-02-14 15:01:50 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 87.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ Java/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,10 +1,8 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int [n];
         
-        for(int i=0; i<n; i++) {
-            answer[i] = num_list[i];
-        }
-        return answer;
+        return Arrays.copyOf(num_list,n);
     }
 }
