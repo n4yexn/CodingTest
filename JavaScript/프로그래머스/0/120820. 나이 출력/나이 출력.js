3125af88c7406cc67a16457d9e0b951833aaa22c
n4yexn <n4yexn@gmail.com>
2026-05-18 14:13:49 +0900

    [level 0] Title: 나이 출력, Time: 0.05 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,11 @@
+function solution(age) {
+    var answer = 0;
+    answer = 2022 + 1 - age;
+//     if(age <= 22)
+//         answer = 2022 - age;
+    
+//     else if(age > 22)
+//         answer = 2023 - age;
+        
+    return answer;
+}
