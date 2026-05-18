bd904e90bd58b3698eed5ef14b9fe4d67ad54f00
n4yexn <n4yexn@gmail.com>
2026-05-18 15:30:14 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 32.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ JavaScript/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,12 +1,3 @@
 function solution(money) {
-    let n = 0;
-    var answer = [n, money];
-    
-    if(money < 5500)
-        answer = [n, money];
-    else 
-        n = Math.floor(money / 5500);
-        money = money - (5500 * n);
-        answer = [n, money];
-    return answer;
+    return [Math.floor(money / 5500), money % 5500];
 }
