a62bd49921666b428bf3cd988d9536d3afde9b14
n4yexn <n4yexn@gmail.com>
2026-05-19 22:54:24 +0900

    [level 0] Title: 짝수의 합, Time: 0.07 ms, Memory: 32.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ JavaScript/프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,13 +1,17 @@
 function solution(n) {
     var answer = 0;
     
-    if(n % 2 != 0)
-        n = n-1;
+//     if(n % 2 != 0)
+//         n = n-1;
         
-    while(n > 0) {
-        answer += n;
-        n = n - 2;
-    }
+//     while(n > 0) {
+//         answer += n;
+//         n = n - 2;
+//     }
     
+//     return answer;
+    
+    for(let i=0; i<=n; i+=2)
+        answer +=i;
     return answer;
 }
