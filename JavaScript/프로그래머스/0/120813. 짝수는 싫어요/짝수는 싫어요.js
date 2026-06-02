e779aa618f512210d6b1c015aa487a49ff14ee68
n4yexn <n4yexn@gmail.com>
2026-06-03 00:24:52 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 32.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,17 +1,8 @@
 function solution(n) {
     var answer = [];
-    if(n % 2 != 0)
-        answer.push(n);
-    
-    while(n > 1){
-        if(n % 2 == 0){ //짝수이면
-            n = n-1;
-            answer.push(n);
-        } else{ //홀수이면
-            n = n-2;
-            answer.push(n);
-        }
+
+    for(let i=1; i<n+1; i+=2){
+        answer.push(i);
     }
-    answer.sort((a, b) => a-b);
     return answer;
 }
