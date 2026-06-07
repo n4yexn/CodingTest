12238d392b0888a18723e3026dd6f6cb4ac59525
n4yexn <n4yexn@gmail.com>
2026-06-07 15:14:41 +0900

    [level 0] Title: A로 B 만들기, Time: 0.35 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,10 @@
+function solution(before, after) {
+    var answer = 0;
+    let arr1 = before.split("").sort().join("");
+    let arr2 = after.split("").sort().join("");
+    
+    if(arr1 == arr2)
+        return 1;
+    else
+        return 0;   
+}
