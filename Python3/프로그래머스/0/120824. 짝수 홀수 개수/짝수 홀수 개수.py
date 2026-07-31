71f29e1a083e219b0716b3765454e8eda0ede519
n4yexn <n4yexn@gmail.com>
2026-08-01 01:30:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,12 +1,10 @@
 def solution(num_list):
-    answer = []
-    e = 0
-    o = 0
+    answer = [0, 0]
+    
     for i in range(len(num_list)):
         if num_list[i] % 2 == 0:
-            e += 1
+            answer[0] += 1
         else:
-            o += 1
-    answer.append(e)
-    answer.append(o)
+            answer[1] += 1
+    
     return answer
