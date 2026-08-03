a797276f213c4330753e9f2ff42f7e46fdfd9e77
n4yexn <n4yexn@gmail.com>
2026-08-03 20:11:43 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,16 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i == 'a':
+            answer += ""
+        elif i == 'e':
+            answer += ""
+        elif i == 'i':
+            answer += ""
+        elif i == 'o':
+            answer += ""
+        elif i == 'u':
+            answer += ""
+        else:
+            answer += i
+    return answer
